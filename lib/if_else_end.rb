66667843c2b1b0e96114bf4d61c35eb897bd4d19
bright-lifecycle-9875 <mairgup@icloud.com>
2020-09-08 17:27:52 +0000

    Done.

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,11 @@
 # Write your solution here
+current_time = Time.now
+current_time = current_time.to_i
+
+if Time.now.to_i % 2 == 0 
+  puts "Even!" 
+  elsif current_time.odd? 
+  puts "Odd!"
+else 
+  puts "idk"
+end
