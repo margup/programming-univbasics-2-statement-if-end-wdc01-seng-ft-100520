b7ac46f9bd94238e16326959c727bfc627bd33ec
bright-lifecycle-9875 <mairgup@icloud.com>
2020-09-08 17:26:43 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -2,4 +2,11 @@
 current_time = Time.now
 current_time = current_time.to_i
 
-if Time.now.to_i 
+if Time.now.to_i % 2 == 0 
+  puts "Even!" 
+  elsif current_time.odd? 
+  puts "Odd!"
+else 
+  puts "idk"
+end
+end
